4032996c9bbeced3dd150d39d1d87be6de0a725d
Behruzbek1212 <begi8354@gmail.com>
2023-09-20 02:08:25 +0500

    dd

--- blog/backend/models.py
+++ blog/backend/models.py
@@ -8,14 +8,22 @@ class Post(models.Model):
     oldingi_narx = models.IntegerField()
     sotuvda_nechta = models.IntegerField()
     tavsif = models.TextField()
-    
-    
-    
-   
+    barcha_malumotlar = models.TextField()
+    operativ_xotira = models.IntegerField(null=True)
+    xotira = models.IntegerField(null=True)
+    operatsion_tizim = models.CharField(max_length=255, null=True)
+    asosiy_kamera = models.IntegerField(null=True)
+    old_kamera = models.IntegerField(null=True)
+    batareya_quvvati = models.IntegerField(null=True)
+    ekran_dioganali = models.FloatField(null=True)
+    protsessor = models.CharField(max_length=255, null=True)
+    brand_nomi = models.CharField(max_length=255, null=True)
+       
     slug = models.SlugField(null='True')
 
     def __str__(self) -> str:
         return self.nomi
+    
     
 class PostImage(models.Model):
     post = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True)
--- blog/backend/views.py
+++ blog/backend/views.py
@@ -1,12 +1,33 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+from django.db.models import Q
+from django.core.paginator import Paginator
 from .models import *
 
 
 def shop(request):
     post = Post.objects.all()
+    
+    if 'q' in request.GET:
+        q = request.GET['q']
+        barchasi = Q(Q(nomi__icontains=q) | Q(hozirgi_narx__icontains=q) | Q(oldingi_narx__icontains=q) | Q(sotuvda_nechta__icontains=q) | 
+        Q(tavsif__icontains=q) | Q(barcha_malumotlar__icontains=q) | Q(operativ_xotira__icontains=q) | Q(xotira__icontains=q) | Q(operatsion_tizim__icontains=q) | 
+        Q(asosiy_kamera__icontains=q) | Q(old_kamera__icontains=q) | Q(batareya_quvvati__icontains=q) | Q(ekran_dioganali__icontains=q) | Q(protsessor__icontains=q) | Q(brand_nomi__icontains=q)) 
+        post = Post.objects.filter(barchasi)
+
+    paginator = Paginator(post, 1)
+    page_number = request.GET.get('page', 1)
+    project_pagination = paginator.get_page(page_number)
+    totalpages = project_pagination.paginator.num_pages
+    page_range = paginator.get_elided_page_range(number=page_number)
+    
     context = {
-        'post': post
+        'post': post,
+        'post': project_pagination,
+        'totalpages': totalpages,
+        'page_range': page_range,
+        'list_pagination': [n+1 for n in range(totalpages)]
     }
+    
     return render(request, 'shop.html',context)
 
 def shop_single(request, slug):
@@ -14,7 +35,8 @@ def shop_single(request, slug):
     rasmlar = PostImage.objects.filter(post=post)
     context = {
         'post': post,
-        'rasmlar': rasmlar
+        'rasmlar': rasmlar,
+
     }
     return render(request, 'shop-single.html', context)
 
